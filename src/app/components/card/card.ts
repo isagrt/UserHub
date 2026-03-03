8c0cf8db191792690a8a9b6e50d005a55743a9a9
isagrt <isadoragoedert05@gmail.com>
2026-03-03 11:20:55 -0300

    adicionando dados em Detalhes

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../models/user';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  standalone: true,  
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './card.html',
   styleUrls: ['./card.css'],
 })
--- src/app/pages/details/details.ts
+++ src/app/pages/details/details.ts
@@ -1,14 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { Container } from '../../components/container/container';
 import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user';
 @Component({
   selector: 'app-details',
   imports: [
-    Container
+    Container,
+    CommonModule
   ],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
 export class Details {
-  @Input() user!: User;
+  user!: User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ){}
+
+  ngOnInit(): void{
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.userService.getUserById(id).subscribe(data => {
+      this.user =data;
+    });
+  }
 }
